3371450682acebacc8140319c0e2be7553640177
JQ <jon.quitoriano@gmail.com>
2025-03-15 00:48:40 -0700

    fix sha hash

--- Casks/siyuan-unlock.rb
+++ Casks/siyuan-unlock.rb
@@ -2,7 +2,7 @@ cask "siyuan-unlock" do
   arch arm: "-arm64"
   
   version "3.1.24"
-  sha256 "f7787ab1ebee97fff3556102af63f73d8189ebd92fd07bb2860bc7c598a89939"
+  sha256 "f94021ba2af243d23d29e217c8b39fe8981a81cda375df1e936055132846fc02"
 
   url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-unlock-v#{version}-mac#{arch}.dmg"
   name "Siyuan Unlock"

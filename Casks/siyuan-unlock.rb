4913fdd727f7a6528dafe5b118401cf26b0fa2c8
JQ <jon.quitoriano@gmail.com>
2025-04-08 10:34:59 -0700

    remove sha checking

--- Casks/siyuan-unlock.rb
+++ Casks/siyuan-unlock.rb
@@ -1,8 +1,8 @@
 cask "siyuan-unlock" do
   arch arm: "-arm64"
   
-  version "3.1.26"
-  sha256 "3eff8124d62246fd25e50eea02f9eefbbf3bc0754b06d602719c9ac7a3a86c55"
+  version :latest
+  sha256 :no_check
 
   url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-unlock-v#{version}-mac#{arch}.dmg"
   name "Siyuan Unlock"
